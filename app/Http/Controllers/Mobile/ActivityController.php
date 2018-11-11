83e3445c38cfacadb9e25a57fce374deb09a19a0
ujehuzair <metalh8@gmail.com>
2018-11-11 17:14:58 +0700

    fix index activity

--- app/Http/Controllers/Mobile/ActivityController.php
+++ app/Http/Controllers/Mobile/ActivityController.php
@@ -42,7 +42,7 @@ class ActivityController extends Controller
                 return response($response, Response::HTTP_NOT_FOUND);
 
             } else {
-                $response = $this->apiLib->singleData($data, []);
+                $response = $this->apiLib->listPaginate($data);
                 return response($response, Response::HTTP_OK);
 
             }
