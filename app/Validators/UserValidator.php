909552c4171e2ad22ebb206740b3e7bb99571665
ujehuzair <metalh8@gmail.com>
2018-12-12 19:34:17 +0700

    delete unnecessary method on Validator

--- app/Validators/UserValidator.php
+++ app/Validators/UserValidator.php
@@ -44,41 +44,4 @@ class UserValidator extends Validator {
         ]);
         return $validator;
     }
-
-    public function validateRegistration(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'username' => 'required|string|unique:users',
-            'full_name' => 'required|string',
-            'phone' => 'unique:users|string',
-            'gender' => 'required',
-            'email' => 'required|string|email|unique:users',
-            'password' => 'required|string|confirmed'
-        ]);
-        return $validator;
-
-    }
-
-    public function validateLogin(Request $request)
-    {
-        $validator = Validator::make($request->only(['email', 'password']), [
-            'email' => 'required',
-            'password' => 'required|string',
-        ]);
-        return $validator;
-    }
-
-    public function validateUpdateProfile(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'username' => 'string|unique:users',
-            'first_name' => 'string',
-            'last_name' => 'string',
-            'phone' => 'unique:users|string',
-            'email' => 'string|email|unique:users',
-            'password' => 'string|confirmed|min:6'
-        ]);
-        return $validator;
-    }
-
 }
